50f4e656a28618a57dfc099bad85d202dd9be083
xgian20 <162388975+xgian20@users.noreply.github.com>
2024-08-24 23:02:23 +0000

    completed learnpack

--- src/app.py
+++ src/app.py
@@ -14,30 +14,24 @@ todos = [
 ]
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
-    json_text = jsonify(todos)
-    return json_text
+def get_todos():
+    return jsonify(todos)
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     todos.append(request_body)
-    print("Incoming request with the following body", request_body)
+    print("Incoming request with the following body:", request_body)
     return jsonify(todos)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    if 0 <= position < len(todos):
+        todos.pop(position)
+    else:
+        return jsonify({"error": "Invalid position"}), 400
+    return jsonify(todos)
 
 
 
@@ -45,6 +39,3 @@ def add_new_todo():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
-
-
-
